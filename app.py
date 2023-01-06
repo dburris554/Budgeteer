fda0b645158f245cabedfa6fd36d3687c98c989d
David Burris <dburris554@yahoo.com>
2023-01-06 06:46:07 +0000

    Data uploads properly

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown('# Good Day')
+# Constants
+COLUMNS = ("Day", "Category", "Amount", "Allocation Name", "Automatic",
+"Paid", "Cleared")
+INDEX = ["Description"]
+
+# Functions
+def initialize_table():
+    st.session_state.df = pd.DataFrame(columns=COLUMNS)
+
+def append_rows(dataframe):
+    st.session_state.df = pd.concat([st.session_state.df, dataframe])
+
+# Initial Session State
+if 'df' not in st.session_state:
+    initialize_table()
+
+# Debugging
+st.write("Session State", st.session_state)
+
+# Streamlit componenets
+st.markdown("# It's a Good Day for Budgeting!")
+data, insights = st.tabs(['Data', 'Insights'])
 
 with st.sidebar:
-    upload = st.file_uploader('Upload a CSV', 'csv')
+    upload = st.file_uploader('Upload budget CSV', 'csv')
     if upload is not None:
-        df = pd.read_csv(upload)
-        st.dataframe(df)
+        df = pd.read_csv(upload, index_col=INDEX)
+        button = st.button('Add rows', on_click=append_rows, args=[df]) # type: ignore
+        if button:
+            st.success('Rows added!', icon="✅")
+
+with data:
+    df = pd.DataFrame(st.session_state.df)
+    st.dataframe(df.style.format(precision=2, thousands=',', na_rep=' '), use_container_width=True)
+    st.button('Clear table', on_click=initialize_table)
