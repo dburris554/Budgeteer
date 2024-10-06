98a721b13389f6cc4ee1867db41a1cda20a91370
David Burris <dburris554@yahoo.com>
2024-10-06 21:12:10 +0000

    Update version

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ with insights_tab:
 
 with about_tab:
     st.markdown('Budgeteer documentation coming soon!')
-    st.markdown('Currently serving `v0.14.2`')
+    st.markdown('Currently serving `v0.14.3`')
 
 # Debugging
 # st.write("Session State", st.session_state)
